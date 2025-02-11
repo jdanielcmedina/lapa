cb618944e7bfc754eb004626fe2ff14f7e0f1c2a
Daniel Medina <j.danielmedina@gmail.com>
2025-02-11 11:39:44 +0000

    Add PHPUnit tests setup

---
+++ tests/Unit/LapaTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Lapa;
+
+class LapaTest extends TestCase
+{
+    public function test_lapa_can_be_instantiated()
+    {
+        $app = new Lapa();
+        $this->assertInstanceOf(Lapa::class, $app);
+    }
+} 
