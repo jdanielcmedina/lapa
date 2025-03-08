47a2515c207f0a34c0ff975742c1186ba860672f
Daniel Medina <j.danielmedina@gmail.com>
2025-03-08 02:11:27 +0000

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,2 +1,27 @@
 <?php
 
+     /**
+     * Protect a route with authentication
+     *
+     * @param callable|null $callback Custom authentication function
+     * @return self
+     * @throws \Exception If authentication fails
+     */
+    public function protect($callback = null) {
+        $isAuthorized = false;
+
+        if (!$callback) {
+            // Default protection - check session
+            $isAuthorized = isset($_SESSION['user']);
+        } else {
+            // Custom protection
+            $isAuthorized = $callback($this);
+        }
+
+        if (!$isAuthorized) {
+            $this->error('Access denied', 403);
+            exit;
+        }
+
+        return $this;
+    }
